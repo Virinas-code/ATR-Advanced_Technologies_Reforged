71f88a19b13704d38ddecc5e0672114ca19804a5
Virinas-code <61283052+Virinas-code@users.noreply.github.com>
2024-07-15 23:09:39 +0000

    build[update-project-description]: WIP script

--- scripts/update_description.py
+++ scripts/update_description.py
@@ -2,6 +2,27 @@
 # -*- coding: utf-8 -*-
 """Update the project description on Modrinth based on the README file."""
 import os
+from pathlib import Path
 
-print(os.environ["modrinth_slug"])
-print(bool(os.environ["modrinth_pat"]))
+import requests
+
+API_KEY: str = os.environ["modrinth_pat"]
+"""TOP SECRET - Modrinth Personnal Access Token"""
+PROJECT_SLUG: str = os.environ["modrinth_slug"]
+"""Modrinth project ID (slug)"""
+
+README_PATH: Path = Path("./README.md")
+
+DEFAULT_HEADERS: dict[str, str] = requests.utils.default_headers()
+"""Default requests headers."""
+
+data: requests.Response = requests.patch(
+  ENDPOINT,
+  json={
+    "body": open(README_PATH).read(),
+  },
+  headers={
+    "Authorization": API_KEY,
+    "User-Agent": "Virinas-code/ATR-Advanced_Technologies_Reforged/1.0 (contact via GitHub Discussions / Issues) " + DEFAULT_HEADERS.get("User-Agent", ""),
+  }
+)
